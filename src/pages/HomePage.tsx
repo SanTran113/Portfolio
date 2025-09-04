84bc54aafefbaec15213e4a2ff1e2745b51b64f6
SanTran113 <113940560+SanTran113@users.noreply.github.com>
2025-09-04 00:35:32 -0700

    added dummy info in homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,7 +6,14 @@ function HomePage() {
       <div className="col-span-3 grid grid-rows-3 text-red-500">
         <div className="bg-gray-800 col-span-3 row-start-2">nav</div>
       </div>
-      
+      <div className="col-span-2 row-span-2">
+        <div className="flex flex-col">
+          <h1 className="text-5xl font-extrabold text-white">San Tran</h1>
+          <p className="text-lg font-bold text-gray-300">Lorem ipsum dolor sit amet, at elit graece eam, eu recusabo deseruisse moderatius vel, cu decore equidem voluptaria eos. No fuisset abhorreant nam, ut essent putant usu. </p>
+          <h2 className="text-3xl font-bold text-white">Work</h2>
+          <h2 className="text-3xl font-bold text-white">About Me</h2>
+        </div>
+      </div>
     </div>
   );
 }
