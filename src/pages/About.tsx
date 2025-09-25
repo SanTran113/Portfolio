c051095ddc606df8e4a78e053ca0c2293c8d0361
SanTran113 <113940560+SanTran113@users.noreply.github.com>
2025-09-24 22:32:08 -0700

    ip about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,18 +5,23 @@ import Leaves from "../assets/leaves.jpeg";
 function About() {
   return (
     <div className="ml-10 mr-10">
-      <div className="text-heading1 text-white font-bold mb-home">
-        About Me!
-      </div>
-      <div className="flex flex-row justify-between overflow-y-hidden">
-        <Mask 
-          className="w-1/2 h-[50vh]" 
-          maskImage={Circle}
-          backgroundImage={Leaves}  
-        />
-        <div className="text-body text-gray-300">
-          
-        </div>
+      <div className="flex flex-col lg:flex-row items-start overflow-y-hidden gap-10 ">
+        <section className="w-full lg:w-1/2 md:w-1/2">
+          <Mask
+            className="h-[50vh]"
+            maskImage={Circle}
+            backgroundImage={Leaves}
+          />
+        </section>
+        <section className="w-full mb-6 text-text-body text-gray-300">
+          <h1 className="text-heading1 text-white font-bold mb-6">Hi, I'm San!</h1>
+          <p>
+            I'm a software engineer based in the Los Angeles Area. I enjoy
+            creating things that live on the internet, whether that be websites,
+            applications, or anything in between. My goal is to build
+            user-friendly and accessible digital experiences.
+          </p>
+        </section>
       </div>
     </div>
   );
