59fbb4624006a22320466d61ae21cd0b8be8f7a3
SanTran113 <113940560+SanTran113@users.noreply.github.com>
2025-09-28 18:14:15 -0700

    added resume

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,7 @@
 import Mask from "../components/Mask";
 import Circle from "../assets/circle.png";
 import Leaves from "../assets/leaves.jpeg";
+import Resume from "../assets/resume.pdf";
 
 function About() {
   return (
@@ -13,14 +14,24 @@ function About() {
             backgroundImage={Leaves}
           />
         </section>
-        <section className="w-full lg:w-1/2 md:w-1/2 mb-6 text-text-body text-gray-300">
-          <h1 className="text-heading1 text-white font-bold mb-6">Hi, I'm San!</h1>
+        <section className="w-full lg:w-1/2 md:w-1/2 text-text-body text-gray-300 space-y-10">
+          <h1 className="text-heading1 text-white font-bold">
+            Hi, I'm San!
+          </h1>
           <p>
             I'm a software engineer based in the Los Angeles Area. I enjoy
             creating things that live on the internet, whether that be websites,
             applications, or anything in between. My goal is to build
             user-friendly and accessible digital experiences.
           </p>
+          <button
+            className="text-contact cursor-pointer hover:text-gray-300 hover:bg-gray-900 p-2 rounded border border-white text-white w-max"
+            onClick={() =>
+              window.open(Resume, "_blank")
+            }
+          >
+            See Resume
+          </button>
         </section>
       </div>
     </div>
