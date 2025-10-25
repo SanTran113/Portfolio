137efdd5811085a3cd5ba7ddda6fab9506efae30
SanTran113 <stran113@calpoly.edu>
2025-10-25 14:50:56 -0700

    remove used tech

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -4,38 +4,35 @@ import Rectangle from "../assets/Rectangle.svg";
 import { projectList } from "../data/projects";
 
 function Works() {
-
   const [project, setProject] = useState(projectList[0]);
 
   const handleProjectClick = (proj: typeof project) => {
     window.open(proj.link, "_blank");
-  }
+  };
 
   return (
-    <div className="flex flex-col lg:flex-row gap-10 item-start h-[calc(100vh-8rem)]">
+    <div className="ml-5 mr-5 lg:ml-10 lg:mr-10 flex flex-col md:flex-row lg:flex-row gap-10 item-start h-[calc(100vh-8rem)]">
       <section className="w-full h-full flex flex-col items-start">
         <Mask
           maskImage={Rectangle}
           backgroundImage={project.coverImg}
-          className="w-full h-full border border-white flex items-center"
+          className="w-full h-full border border-white flex items-center position-absolute"
         />
         {/* <div className="text-white text-body mt-5 mb-3 tracking-wide">{project.skillsUsed}</div> */}
         {/* <hr className="border-b border-white w-full" /> */}
       </section>
-      <section className="w-full flex flex-col ml-10 mr-10">
-        <div className="text-white text-heading1 font-bold mb-5">Projects</div>
-        <ul className="text-heading2 font-medium flex flex-col ml-5 gap-2 lg:gap-5 md:gap-0">
+      <section className="w-full flex flex-col ">
+        <div className="text-white text-heading1 font-bold ">Projects</div>
+        <ul className="flex flex-col ml-5">
           {projectList.map((proj) => (
             <button
               key={proj.name}
-              className="group text-white text-left cursor-pointer hover:text-gray-300 hover:outline-3 hover/see:visible rounded flex flex-row justify-between items-center p-3 lg:p-5"
+              className="group text-left cursor-pointer hover:opacity-80 hover:outline-1 hover/see:visible rounded flex flex-row justify-between items-center p-3 lg:p-5"
               onMouseEnter={() => setProject(proj)}
               onClick={() => handleProjectClick(proj)}
             >
-              {proj.name}
-              <div className="invisible group-hover:visible text-sm text-gray-400 text-body">
-                See Project &gt;
-              </div>
+              <h1 className="text-white text-heading2 font-medium">{proj.name} </h1>
+              <body className="text-gray-300 text-heading3">{proj.type}</body>
             </button>
           ))}
         </ul>
