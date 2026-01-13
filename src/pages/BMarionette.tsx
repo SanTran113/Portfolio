4cd5d3a69fa75a831a2fa46317db2ffdd976edec
SanTran113 <113940560+SanTran113@users.noreply.github.com>
2026-01-12 22:53:19 -0800

    incorporate Marionette model assets

--- src/pages/BMarionette.tsx
+++ src/pages/BMarionette.tsx
@@ -3,16 +3,27 @@ import ProjectHeader from "../components/ProjectHeader";
 import { projectList } from "../data/projectsList";
 
 import BMCover from "../assets/BMarionette/BMCover.png";
+import BM_eyes from "../assets/BMarionette/BM_eyes.png";
 import MFront from "../assets/BMarionette/Marionette_front.png";
 import MSide from "../assets/BMarionette/Marionette_side.png";
+import BM_sculpt from "../assets/BMarionette/BM_sculpting.png";
+import BM_topoFace from "../assets/BMarionette/BM_topologyFace.png";
+import BM_topoBody from "../assets/BMarionette/BM_topologyBody.png";
+import BM_UVMap from "../assets/BMarionette/BM_UVMap.png";
+import musicBox from "../assets/BMarionette/musicBox_UVMap.png";
+import MBTex from "../assets/BMarionette/BMTexture.png";
+import musicBoxTex from "../assets/BMarionette/musicBoxTexture.png";
+import BMLight from "../assets/BMarionette/BMLighting.png";
+import BM_Bow from "../assets/BMarionette/BM_bow scene.jpg";
+import BM_dance from "../assets/BMarionette/BM_dance_scene.jpg";
 
 function BMarionette() {
   const [proj] = useState(projectList[3]);
 
   return (
-    <div>
+    <div className="w-full mb-[10%]">
       <ProjectHeader
-        coverImg={BMCover}
+        coverImg={BM_eyes}
         projectName={proj.name}
         year={"September 2025 - December 2025"}
         role="3D Modeler and Animator"
@@ -24,7 +35,10 @@ function BMarionette() {
             Animation
             <iframe
               className="w-full aspect-video mt-2"
-              src="www.youtube.com"
+              src="https://www.youtube.com/embed/ePNLOM37oxA"
+              title="Butterfly Marionette Animation"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
             ></iframe>
           </section>
 
@@ -41,12 +55,18 @@ function BMarionette() {
           </section>
 
           <section className="text-heading2 text-white font-bold">
-            Topology
+            Sculpting & Topology
             <p className="text-body font-normal">
-              After sketching, I began modeling the character in a T-posein
+              After sketching, I began modeling the character in a T-poseing
               Maya, and made sure retopologize often. Throughout the process, I
               focused keenly on anatomy, shapes, and proporations.
             </p>
+            <div className="flex flex-row mt-2 max-w-1/2">
+              {" "}
+              <img src={BM_sculpt} alt="Sculpting" className="mt-2" />
+              <img src={BM_topoBody} alt="Body Topology" className="mt-2" />
+            </div>
+            <img src={BM_topoFace} alt="Face Topology" className="mt-2" />
           </section>
 
           <section className="text-heading2 text-white font-bold">
@@ -57,18 +77,18 @@ function BMarionette() {
               shell flat, for easier texturing in the future. Using the
               checkered feature, I made sure the unwrap could correctly project
               the texture. Lastly, I organize the UV layout by scaling and
-              arranging the UV shells.
+              arranging the UV shells. On the left is the UV unwrap of the
+              marionette and on the right is the music box.
             </p>
-          </section>
-
-          <section className="text-heading2 text-white font-bold">
-            Quick Rig and Posing
-            <p className="text-body font-normal">
-              Unfortuntly, the quick rig dedected the wings as arms, so I had to
-              manually adjust the bones to fit the wings. After adjusting the
-              bones, I went into cleaning up the weight painting to ensure
-              proper deformation when posing the character.
-            </p>
+            <div className="flex flex-wrap space-x-2 mt-2 w-full">
+              {" "}
+              <img src={BM_UVMap} alt="UV Mapping" className="mt-2 max-w-lg" />
+              <img
+                src={musicBox}
+                alt="Music Box UV Mapping"
+                className="mt-2 max-w-lg"
+              />
+            </div>
           </section>
 
           <section className="text-heading2 text-white font-bold">
@@ -82,7 +102,21 @@ function BMarionette() {
               details of the eyes. The main texture on the music box was an
               online texture I found online, linked below!
             </p>
+            <div className="flex flex-row mt-2 max-w-1/2">
+              <img src={musicBoxTex} alt="Music Box Texture" className="mt-2" />
+              <img src={MBTex} alt="Marionette Texture" className="mt-2" />
+            </div>
             <p>LINK</p>
+          </section>
+
+          <section className="text-heading2 text-white font-bold">
+            Quick Rig and Posing
+            <p className="text-body font-normal">
+              Unfortuntly, the quick rig dedected the wings as arms, so I had to
+              manually adjust the bones to fit the wings. After adjusting the
+              bones, I went into cleaning up the weight painting to ensure
+              proper deformation when posing the character.
+            </p>
           </section>
 
           <section className="text-heading2 text-white font-bold">
@@ -91,13 +125,28 @@ function BMarionette() {
               Lighting was the most difficult for me, as I had to learn
               different lighting techniques that I have not worked with before.
               I played around with the lighting with the goal of having a
-              spotlight affect on the marionette. Using ambience occlusion I was
-              able to bring the piece to life.
+              spotlight affect on the marionette, leading to a dramatisized
+              3-point lighting. Using ambience occlusion I was able to bring the
+              piece to life.
             </p>
+            <img src={BMLight} alt="Lighting Setup" className="mt-2" />
+          </section>
+
+          <section className="text-heading2 text-white font-bold">
+            Animating
+            <p className="text-body font-normal">
+              For animation I used keyframing in Maya. I started by blocking out
+              the main poses, then refining the in-betweens to create a smooth
+              motion. I focused on timing and spacing to ensure the animation
+              felt natural and expressive.
+            </p>
+            <img src={BMLight} alt="Lighting Setup" className="mt-2" />
           </section>
 
           <section className="text-heading2 text-white font-bold">
             Final Rendering
+            <img src={BM_Bow} alt="Final Bow Scene" className="mt-2" />
+            <img src={BM_dance} alt="Final Dance Scene" className="mt-2" />
           </section>
         </div>
       </div>
