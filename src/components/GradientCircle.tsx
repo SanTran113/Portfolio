9a7c95da9c18aa7b6fdc288aabe6f0d6515297ed
SanTran113 <stran113@calpoly.edu>
2026-03-30 10:56:58 -0700

    fix: nav works

--- src/components/GradientCircle.tsx
+++ src/components/GradientCircle.tsx
@@ -3,7 +3,7 @@ export function GradientCircle({ className }: { className?: string }) {
 
   return (
     <div
-      className={`rounded-full w-screen aspect-square animate-pulse opacity-70 ${className}`}
+      className={`rounded-full w-full aspect-square animate-pulse opacity-70 ${className}`}
       style={{
         background: `radial-gradient(50% 50% at 50% 50%, #62536B 0%, transparent 100%)`,
       }}
