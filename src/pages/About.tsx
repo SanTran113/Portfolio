c7bca4422052a675ab2714dbd7504d1136fb944d
SanTran113 <stran113@calpoly.edu>
2026-03-30 00:53:10 -0700

    chore: add in grad circles

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,47 +2,53 @@ import Mask from "../components/Mask";
 import Circle from "../assets/circle_arylic.png";
 import Profile from "../assets/profile.jpg";
 import Resume from "../assets/resume.pdf";
+import { GradientCircle } from "../components/GradientCircle";
 
 function About() {
   return (
-    <div className="pl-10 pr-10">
-      <div className="flex flex-col lg:flex-row xm:flex-row items-center lg:gap-10 xm:gap-5 w-full lg:h-[calc(100vh-6rem)]">
-        <section className="w-full h-full lg:w-2/5 xm:w-2/5 md:w-3/4 flex justify-center items-center">
-          <Mask
-            className="w-full size-aboutImg"
-            maskImage={Circle}
-            backgroundImage={Profile}
-          />
-        </section>
-        <section className="w-full lg:w-1/2 xm:w-1/2 md:w-3/4 text-text-body text-gray-300 space-y-5 lg:space-y-8">
-          <h1 className="text-heading1 text-white font-bold">Hi, I'm San!</h1>
-          <p className="text-body">
-            I'm a software engineer based in the Los Angeles Area. I enjoy
-            creating things that live on the internet, whether that be websites,
-            applications, or anything in between. My goal is to build
-            user-friendly and accessible digital experiences.
-          </p>
-          <div>
-            <h1 className="text-heading2 text-white font-bold">Skills</h1>
-            {/* TODO: Some of the screens only flex typescript and does not evenly split the lis */}
-            <ul className="text-body list-disc list-inside space-y-2 pl-5 flex flex-wrap space-x-5 lg:space-x-20">
-              <li className="w-30 lg:w-1/4 xm:w-1/3">JavaScript</li>
-              <li className="w-30 lg:w-1/4 xm:w-1/3">React</li>
-              <li className="w-30 lg:w-1/4 xm:w-1/3">Node.js</li>
-              <li className="w-30 lg:w-1/4 xm:w-1/3">CSS</li>
-              <li className="w-30 lg:w-1/4 xm:w-1/3">HTML</li>
-              <li className="w-30 lg:w-1/4 xm:w-1/3">TypeScript</li>
-            </ul>
-          </div>
-          <button
-            className="text-body cursor-pointer hover:bg-gray-200 p-2 rounded bg-white text-[#1a1a1a] w-full mb-10"
-            onClick={() => window.open(Resume, "_blank")}
-          >
-            See Resume
-          </button>
-        </section>
+    <>
+      <div className="pl-10 pr-10">
+        <div className="flex flex-col lg:flex-row xm:flex-row items-center lg:gap-10 xm:gap-5 w-full lg:h-[calc(100vh-6rem)]">
+          <section className="w-full h-full lg:w-2/5 xm:w-2/5 md:w-3/4 flex justify-center items-center">
+            <Mask
+              className="w-full size-aboutImg"
+              maskImage={Circle}
+              backgroundImage={Profile}
+            />
+          </section>
+          <section className="w-full lg:w-1/2 xm:w-1/2 md:w-3/4 text-text-body text-gray-300 space-y-5 lg:space-y-8">
+            <h1 className="text-heading1 text-white font-bold">Hi, I'm San!</h1>
+            <p className="text-body">
+              I'm a software engineer based in the Los Angeles Area. I enjoy
+              creating things that live on the internet, whether that be
+              websites, applications, or anything in between. My goal is to
+              build user-friendly and accessible digital experiences.
+            </p>
+            <div>
+              <h1 className="text-heading2 text-white font-bold">Skills</h1>
+              {/* TODO: Some of the screens only flex typescript and does not evenly split the lis */}
+              <ul className="text-body list-disc list-inside space-y-2 pl-5 flex flex-wrap space-x-5 lg:space-x-20">
+                <li className="w-30 lg:w-1/4 xm:w-1/3">JavaScript</li>
+                <li className="w-30 lg:w-1/4 xm:w-1/3">React</li>
+                <li className="w-30 lg:w-1/4 xm:w-1/3">Node.js</li>
+                <li className="w-30 lg:w-1/4 xm:w-1/3">CSS</li>
+                <li className="w-30 lg:w-1/4 xm:w-1/3">HTML</li>
+                <li className="w-30 lg:w-1/4 xm:w-1/3">TypeScript</li>
+              </ul>
+            </div>
+            <button
+              className="text-body cursor-pointer hover:bg-gray-200 p-2 rounded bg-white text-[#1a1a1a] w-full mb-10"
+              onClick={() => window.open(Resume, "_blank")}
+            >
+              See Resume
+            </button>
+          </section>
+        </div>
       </div>
-    </div>
+      <section className="fixed -z-10 -translate-y-210 lg:-translate-x-100 lg:-translate-y-250">
+        <GradientCircle />
+      </section>
+    </>
   );
 }
 
--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -29,59 +29,63 @@ function Works() {
   };
 
   return (
-    <div className="px-5 lg:px-10 flex flex-col md:flex-row lg:flex-row gap-10 item-start h-[calc(100vh-8rem)]">
-      <section className="flex flex-col w-full ">
-        <img
-          className="pt-6 w-full min-h-2/5 lg:min-h-7/8 object-cover bg-no-repeat"
-          src={project.coverImg}
-        />
-        <div className="text-white text-body font-semibold mt-5">
-          {project.skillsUsed}
-        </div>
-        <hr className="w-full border-gray-300 mt-2 mb-5" />
-      </section>
-      <section className="w-full h-full flex flex-col ">
-        <div className="text-white text-heading1 font-bold mt-5">Projects</div>
-        <div className="w-full text-white text-heading2 font-normal mt-[3%] mb-[3%] flex flex-row">
-          <button
-            className={tabClassName("dev")}
-            onClick={() => setSection("dev")}
-          >
-            Development
-          </button>
-          <button
-            className={tabClassName("design")}
-            onClick={() => setSection("design")}
-          >
-            Design
-          </button>
-        </div>
-        <ul className="flex flex-col ml-5">
-          {projectList
-            .filter(
-              (proj) =>
-                proj.workType === (type?.type || "web") &&
-                proj.sectionType?.includes(section),
-            )
-            .map((proj) => (
-              <button
-                key={proj.name}
-                className="group text-left cursor-pointer hover:opacity-80 hover:outline-1 hover:outline-white flex flex-row justify-between items-center p-2 lg:p-4 space-y-1"
-                onMouseEnter={() => setProject(proj)}
-                onClick={() => handleProjectClick(proj)}
-              >
-                <h1 className="text-white text-heading2 font-medium">
-                  {proj.name}
-                </h1>
-                <div className="text-gray-300 text-body">{proj.type}</div>
-              </button>
-            ))}
-        </ul>
-      </section>
-      {/* <section className="fixed inset-0 -z-10 translate-y-0 lg:-translate-x-80">
+    <>
+      <div className="px-5 lg:px-10 flex flex-col md:flex-row lg:flex-row gap-10 item-start h-[calc(100vh-8rem)]">
+        <section className="flex flex-col w-full ">
+          <img
+            className="pt-6 w-full min-h-2/5 lg:min-h-7/8 object-cover bg-no-repeat"
+            src={project.coverImg}
+          />
+          <div className="text-white text-body font-semibold mt-5">
+            {project.skillsUsed}
+          </div>
+          <hr className="w-full border-gray-300 mt-2 mb-5" />
+        </section>
+        <section className="w-full h-full flex flex-col ">
+          <div className="text-white text-heading1 font-bold mt-5">
+            Projects
+          </div>
+          <div className="w-full text-white text-heading2 font-normal mt-[3%] mb-[3%] flex flex-row">
+            <button
+              className={tabClassName("dev")}
+              onClick={() => setSection("dev")}
+            >
+              Development
+            </button>
+            <button
+              className={tabClassName("design")}
+              onClick={() => setSection("design")}
+            >
+              Design
+            </button>
+          </div>
+          <ul className="flex flex-col ml-5">
+            {projectList
+              .filter(
+                (proj) =>
+                  proj.workType === (type?.type || "web") &&
+                  proj.sectionType?.includes(section),
+              )
+              .map((proj) => (
+                <button
+                  key={proj.name}
+                  className="group text-left cursor-pointer hover:opacity-80 hover:outline-1 hover:outline-white flex flex-row justify-between items-center p-2 lg:p-4 space-y-1"
+                  onMouseEnter={() => setProject(proj)}
+                  onClick={() => handleProjectClick(proj)}
+                >
+                  <h1 className="text-white text-heading2 font-medium">
+                    {proj.name}
+                  </h1>
+                  <div className="text-gray-300 text-body">{proj.type}</div>
+                </button>
+              ))}
+          </ul>
+        </section>
+      </div>
+      <section className="fixed -z-10 -translate-y-180 lg:-translate-x-50 lg:-translate-y-250">
         <GradientCircle />
-      </section> */}
-    </div>
+      </section>
+    </>
   );
 }
 
