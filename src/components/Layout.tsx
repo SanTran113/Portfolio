7e373c76e09fb3b2dddd2ffb76262d1921aaac61
SanTran113 <stran113@calpoly.edu>
2026-03-31 22:45:22 -0700

    fix: disable nav at homepage

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 import { ParticlesBg } from "./ParticlesBg";
 
 function Layout() {
+  const { pathname } = useLocation();
+  const hideNav = pathname === "/";
+
   return (
     <>
-      <div className="">
+      <div className={hideNav ? "hidden" : "block"}>
         <Navbar />
         <ParticlesBg />
       </div>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,16 +3,16 @@ import { GradientCircle } from "../components/GradientCircle";
 function HomePage() {
   return (
     <>
-      <div className="w-full flex flex-row justify-center items-center gap-10 h-[calc(100vh-6rem)] p-10">
+      <div className="w-full flex flex-col lg:flex-row justify-center items-center gap-10 h-[calc(100vh-6rem)] p-5 lg:p-10">
         <a
           href="/web"
-          className="w-3/4 h-3/4 outline-1 outline-white text-white text-5xl flex justify-center items-center hover:bg-white hover:text-[#2B2C3C] cursor-pointer"
+          className="w-3/4 h-3/4 p-5 outline-1 outline-white text-white text-heading1 flex justify-center items-center hover:bg-white hover:text-[#2B2C3C] cursor-pointer"
         >
           Web Development
         </a>
         <a
           href="/game"
-          className="w-3/4 h-3/4 outline-1 outline-white text-white text-5xl flex justify-center items-center hover:bg-white hover:text-[#2B2C3C] cursor-pointer"
+          className="w-3/4 h-3/4 p-5 outline-1 outline-white text-white text-heading1 flex justify-center items-center hover:bg-white hover:text-[#2B2C3C] cursor-pointer"
         >
           Game Development
         </a>
