b6440e0910d3b1607ea8f761184ad3eb7ea25cc5
SanTran113 <stran113@calpoly.edu>
2026-03-31 22:46:30 -0700

    fix: use Link in homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,22 @@
+import { Link } from "react-router-dom";
 import { GradientCircle } from "../components/GradientCircle";
 
 function HomePage() {
   return (
     <>
       <div className="w-full flex flex-col lg:flex-row justify-center items-center gap-10 h-[calc(100vh-6rem)] p-5 lg:p-10">
-        <a
-          href="/web"
+        <Link
+          to="/web"
           className="w-3/4 h-3/4 p-5 outline-1 outline-white text-white text-heading1 flex justify-center items-center hover:bg-white hover:text-[#2B2C3C] cursor-pointer"
         >
           Web Development
-        </a>
-        <a
-          href="/game"
+        </Link>
+        <Link
+          to="/game"
           className="w-3/4 h-3/4 p-5 outline-1 outline-white text-white text-heading1 flex justify-center items-center hover:bg-white hover:text-[#2B2C3C] cursor-pointer"
         >
           Game Development
-        </a>
+        </Link>
       </div>
       <section className="fixed -z-10 inset-0 -translate-y-130">
         <GradientCircle />
