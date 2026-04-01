0135c3ae6695c7a0183bdcd4bcbfecf2d6975fd8
SanTran113 <stran113@calpoly.edu>
2026-03-31 21:36:26 -0700

    make about section dynamic

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,74 @@
+import { useParams } from "react-router-dom";
 import Mask from "../components/Mask";
 import Circle from "../assets/circle_arylic.png";
 import Profile from "../assets/profile.jpg";
-import Resume from "../assets/resume.pdf";
+import SERes from "../assets/SE Resume.pdf";
+import GameRes from "../assets/Games Resume.pdf";
 import { GradientCircle } from "../components/GradientCircle";
+import type { WorkType } from "../data/WorkType";
 
 function About() {
+  const type = useParams<{ type: WorkType }>();
+
+  function getBio() {
+    if (type?.type === "web") {
+      return (
+        <p className="text-body">
+          I'm a software engineer based in the Los Angeles Area. I enjoy
+          creating things that live on the internet, whether that be websites,
+          applications, or anything in between. My goal is to build
+          user-friendly and accessible digital experiences.
+        </p>
+      );
+    } else if (type?.type === "game") {
+      return (
+        <p className="text-body">
+          I'm a game developer based in the Los Angeles Area. I enjoy
+          makeing games and interactive experiences that are engaging and fun. 
+          My goal is to create games that are not only enjoyable to play, but also visually
+          appealing and immersive!
+        </p>
+      );
+    }
+  }
+
+  function getSkills() {
+    if (type?.type === "web") {
+      return (
+        <ul className="text-body list-disc list-inside space-y-2 pl-5 flex flex-wrap space-x-5 lg:space-x-20">
+          <li className="w-30 lg:w-1/4 xm:w-1/3">JavaScript</li>
+          <li className="w-30 lg:w-1/4 xm:w-1/3">React</li>
+          <li className="w-30 lg:w-1/4 xm:w-1/3">Node.js</li>
+          <li className="w-30 lg:w-1/4 xm:w-1/3">CSS</li>
+          <li className="w-30 lg:w-1/4 xm:w-1/3">HTML</li>
+          <li className="w-30 lg:w-1/4 xm:w-1/3">TypeScript</li>
+        </ul>
+      );
+    } else if (type?.type === "game") {
+      return (
+        <ul className="text-body list-disc list-inside space-y-2 pl-5 flex flex-wrap space-x-5 lg:space-x-20">
+          <li className="w-30 lg:w-1/4 xm:w-1/3">C#</li>
+          <li className="w-30 lg:w-1/4 xm:w-1/3">C++</li>
+          <li className="w-30 lg:w-1/4 xm:w-1/3">Unity</li>
+          <li className="w-30 lg:w-1/4 xm:w-1/3">Unreal</li>
+          <li className="w-30 lg:w-1/4 xm:w-1/3">Blender</li>
+          <li className="w-30 lg:w-1/4 xm:w-1/3">Maya</li>
+          <li className="w-30 lg:w-1/4 xm:w-1/3">Substance Painter</li>
+          <li className="w-30 lg:w-1/4 xm:w-1/3">Adobe Photoshop</li>
+          <li className="w-30 lg:w-1/4 xm:w-1/3">Adobe Illustrator</li>
+        </ul>
+      );
+    }
+  }
+
+  function getResumeLink() {
+    if (type?.type === "web") {
+      return SERes;
+    } else if (type?.type === "game") {
+      return GameRes;
+    }
+  }
+
   return (
     <>
       <div className="pl-10 pr-10">
@@ -18,27 +82,14 @@ function About() {
           </section>
           <section className="w-full lg:w-1/2 xm:w-1/2 md:w-3/4 text-text-body text-gray-300 space-y-5 lg:space-y-8">
             <h1 className="text-heading1 text-white font-bold">Hi, I'm San!</h1>
-            <p className="text-body">
-              I'm a software engineer based in the Los Angeles Area. I enjoy
-              creating things that live on the internet, whether that be
-              websites, applications, or anything in between. My goal is to
-              build user-friendly and accessible digital experiences.
-            </p>
+            {getBio()}
             <div>
               <h1 className="text-heading2 text-white font-bold">Skills</h1>
-              {/* TODO: Some of the screens only flex typescript and does not evenly split the lis */}
-              <ul className="text-body list-disc list-inside space-y-2 pl-5 flex flex-wrap space-x-5 lg:space-x-20">
-                <li className="w-30 lg:w-1/4 xm:w-1/3">JavaScript</li>
-                <li className="w-30 lg:w-1/4 xm:w-1/3">React</li>
-                <li className="w-30 lg:w-1/4 xm:w-1/3">Node.js</li>
-                <li className="w-30 lg:w-1/4 xm:w-1/3">CSS</li>
-                <li className="w-30 lg:w-1/4 xm:w-1/3">HTML</li>
-                <li className="w-30 lg:w-1/4 xm:w-1/3">TypeScript</li>
-              </ul>
+              {getSkills()}
             </div>
             <button
               className="text-body cursor-pointer p-2 hover:bg-white hover:text-[#2B2C3C] w-full mb-10 outline-white outline-1"
-              onClick={() => window.open(Resume, "_blank")}
+              onClick={() => window.open(getResumeLink(), "_blank")}
             >
               See Resume
             </button>
--- src/pages/Game/GameHome.tsx
+++ src/pages/Game/GameHome.tsx
@@ -1,45 +1,20 @@
-import Mask from "../../components/Mask";
-import Paint from "../../assets/circle_arylic.png";
-import Clouds from "../../assets/pinkClouds.jpeg";
-import { Link } from "react-router-dom";
+import { GradientCircle } from "../../components/GradientCircle";
+import Button from "../../components/Button";
 
 function WebHome() {
-
   return (
-    <div className="pl-10 pr-10 overflow-hidden h-[calc(100vh-6rem)]">
-      <h1 className="text-title font-extrabold text-white mb-home">San Tran</h1>
-      <div className="flex flex-col xm:flex-row lg:flex-row gap-4 h-full">
-        <div className="w-full flex flex-col space-y-10">
-          <section className="space-y-6">
-            <p className="text-body lg:font-semibold text-gray-300">
-              Hellow! I'm a developer and designer with a passion for capturing
-              life through my projects, whether that be through web or games!
-            </p>
-            <section className="text-heading1 font-bold text-white">
-              <nav className="flex flex-col justify-between ">
-                <ul className="flex flex-col space-x-4">
-                  <li>
-                    <Link to="/game/works">Works</Link>
-                  </li>
-                  <li>
-                    <Link to="/game/aboutme">About</Link>
-                  </li>
-                  <li>
-                    <Link to="/game/contact">Contact</Link>
-                  </li>
-                </ul>
-              </nav>
-            </section>
-          </section>
-        </div>
-        <section className="w-full relative h-1/2">
-          <Mask
-            className="place-self-center size-homeImg inline-block absolute lg:-translate-y-2/5 lg:left-1/12"
-            maskImage={Paint}
-            backgroundImage={Clouds}
-          />
+    <div className="w-full h-[calc(100vh-6rem)] relative flex justify-center items-center">
+      <section className="absolute flex flex-col justify-center items-center">
+        <h1 className="text-title font-light text-white mb-home flex justify-center">
+          San Tran
+        </h1>
+        <section className="text-heading1 font-bold text-white flex justify-center items-center">
+          <Button label="See Projects" buttonLink="/game/works" icon={true} />
         </section>
-      </div>
+      </section>
+      <section className="fixed -z-10 inset-0 -translate-y-130">
+        <GradientCircle />
+      </section>
     </div>
   );
 }
