f8e6c5d6eb487a964fcc145584f2d1255c4e89fc
SanTran113 <stran113@calpoly.edu>
2026-09-18 15:02:13 -0700

    add: inner dot to cursor

--- src/motion/cursor.tsx
+++ src/motion/cursor.tsx
@@ -7,9 +7,16 @@ export default function Cursor() {
 
   const mouseX = useMotionValue(0);
   const mouseY = useMotionValue(0);
-  const x = useSpring(mouseX, { damping: 25, stiffness: 300 });
-  const y = useSpring(mouseY, { damping: 25, stiffness: 300 });
 
+  //   cursor ring movement transition
+  const ringX = useSpring(mouseX, { damping: 25, stiffness: 300 });
+  const ringY = useSpring(mouseY, { damping: 25, stiffness: 300 });
+
+  // cursor dot movement transition
+  const dotX = useSpring(mouseX, { damping: 40, stiffness: 800 });
+  const dotY = useSpring(mouseY, { damping: 40, stiffness: 800 });
+
+  //   Detecting movement and pressing
   useEffect(() => {
     const move = (e) => {
       mouseX.set(e.clientX);
@@ -30,29 +37,47 @@ export default function Cursor() {
   }, []);
 
   return (
-    <motion.div
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        x,
-        y,
-        width: 20,
-        height: 20,
-        borderRadius: "50%",
-        borderWidth: 1,
-        borderColor: "white",
-        background: "none",
-        pointerEvents: "none",
-        translateX: "-50%",
-        translateY: "-50%",
-      }}
-      animate={{
-        width: isPressing ? 30 : isHovering ? 40 : 20,
-        height: isPressing ? 30 : isHovering ? 40 : 20,
-        background: isPressing ? "white" : isHovering ? "white" : "none",
-      }}
-      transition={{ type: "spring", stiffness: 300, damping: 20 }}
-    />
+    <>
+      {/* outer ring */}
+      <motion.div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          x: ringX,
+          y: ringY,
+          borderRadius: "50%",
+          border: "1px solid white",
+          pointerEvents: "none",
+          translateX: "-50%",
+          translateY: "-50%",
+        }}
+        animate={{
+          width: isPressing ? 40 : isHovering ? 40 : 25,
+          height: isPressing ? 40 : isHovering ? 40 : 25,
+          background: isPressing ? "none" : isHovering ? "white" : "none",
+        }}
+        transition={{ type: "spring", stiffness: 300, damping: 20 }}
+      />
+
+      {/* center dot */}
+      <motion.div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          x: dotX,
+          y: dotY,
+          width: 3,
+          height: 3,
+          borderRadius: "50%",
+          background: "white",
+          pointerEvents: "none",
+          translateX: "-50%",
+          translateY: "-50%",
+        }}
+        transition={{ duration: 0.15 }}
+      />
+    </>
   );
 }
