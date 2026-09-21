5aaab8be13316f80db2376545b9dd697e42341c6
SanTran113 <stran113@calpoly.edu>
2026-09-21 11:41:23 -0700

    move cursor to components

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
 
 import Layout from "./components/Layout.tsx";
-import Cursor from "./motion/Cursor.tsx";
+import Cursor from "./components/Cursor.tsx";
 
 import HomePage from "./pages/HomePage.tsx";
 import Contact from "./pages/Contact.tsx";
