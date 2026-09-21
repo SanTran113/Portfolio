92ab897c9890ffe59b205b58ef23ab33294b23d6
SanTran113 <stran113@calpoly.edu>
2026-09-20 22:35:49 -0700

    fix: icon centering

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,7 +13,7 @@ interface ButtonProps {
 
 const varientStyles: Record<Varient, string> = {
   primary:
-    "relative text-heading2 outline-white flex flex-row justify-between items-center p-3 lg:p-4 space-y-1 hover:text-[#2B2C3C]",
+    "relative text-heading2 outline-white flex flex-row justify-between items-center p-3 lg:p-4 hover:text-[#2B2C3C]",
   fullPrimary:
     "relative text-contact hover:text-[#2B2C3C] p-2 border-white w-full",
 };
@@ -30,13 +30,14 @@ export default function Button({
   const [isHovering, setIsHovering] = useState(false);
 
   const content = (
-    <div className="flex flex-row justify-between items-center w-full">
+    <>
       {label}
       {icon && (
-        <i className="fi fi-rr-angle-small-right flex justify-center"></i>
+        <i className="fi fi-rr-angle-small-right flex items-center justify-center"/>
       )}
-    </div>
+    </>
   );
+
 
   const buttonBg = (
     <motion.span
